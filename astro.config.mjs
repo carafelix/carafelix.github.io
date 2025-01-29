8bf7f0945a21b4548bacc02d58a628466e22e115
Hero Protagonist <heroprotagonist32@gmail.com>
2025-01-29 15:26:44 -0300

    feat: add moving squares

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@astrojs/react'
 
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
     site: 'https://heroprotagonist.is-a.dev',
-    integrations: [react()],
     compressHTML: true,
 })
